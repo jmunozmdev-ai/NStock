f8fa9204013b7d0019da2f8cd442e17bef021fbc
Javier alberto muñoz <jmunozm.dev@gmail.com>
2026-06-26 12:56:03 -0400

    Delete Producto/nstock directory

--- Producto/nstock/src/main/java/com/nstock/nstock/repository/DetalleVentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nstock.nstock.repository;
-
-import com.nstock.nstock.entity.DetalleVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Integer> {
-}
--- Producto/nstock/src/main/java/com/nstock/nstock/repository/VentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nstock.nstock.repository;
-
-import com.nstock.nstock.entity.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VentaRepository extends JpaRepository<Venta, Integer> {
-}
